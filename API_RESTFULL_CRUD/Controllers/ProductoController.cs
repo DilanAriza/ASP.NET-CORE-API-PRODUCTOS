20e1bf647a56d4653ce29f723ea507a51bc62f49
DilanAriza <dilansantiagoariza@gmail.com>
2021-06-30 11:00:36 -0500

    Agregando todo lo basico de la API, ya tiene conección a la base de datos y aparte ya cuenta con un metodo de GET para los productos, faltan los demás verbos HTTP

---
+++ API_RESTFULL_CRUD/Controllers/ProductoController.cs
@@ -0,0 +1,59 @@
+﻿using API_RESTFULL_CRUD.Context;
+using API_RESTFULL_CRUD.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_RESTFULL_CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        // Propiedad del contexto que le pasamaos al injectar el AppDbContext
+        private readonly AppDbContext context;
+
+        //Constructor del controller, aquí le injectamos el contexto de la app
+        public ProductoController(AppDbContext context)
+        {
+            //  Asignando la propiedad y pasandola al constructor
+            this.context = context;
+        }
+
+        // GET: api/<ProductoController>
+        [HttpGet]
+        public IEnumerable<Producto> Get()
+        {
+            return context.Producto.ToList();
+        }
+
+        // GET api/<ProductoController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProductoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ API_RESTFULL_CRUD/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_RESTFULL_CRUD.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        //private static readonly string[] Summaries = new[]
+        //{
+        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        //};
+
+        //private readonly ILogger<ValuesController> _logger;
+
+        //public ValuesController(ILogger<ValuesController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value 1", "value 2" };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value " + id;
+        }
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+    }
+}
